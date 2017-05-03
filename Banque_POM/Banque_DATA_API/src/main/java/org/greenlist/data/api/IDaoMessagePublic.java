f325e5b639eb4c578caa1990f15a54d31b0639bd
Rodrigue <rodrigue@mail.fr>
2017-05-03 10:02:13 +0200

    Message public en cours

--- Banque_POM/Banque_DATA_API/src/main/java/org/greenlist/data/api/IDaoMessagePublic.java
+++ Banque_POM/Banque_DATA_API/src/main/java/org/greenlist/data/api/IDaoMessagePublic.java
@@ -1,9 +1,12 @@
 package org.greenlist.data.api;
 
+import java.util.List;
 import org.greenlist.entity.Messagepublic;
 import org.greenlist.entity.Objet;
 
 public interface IDaoMessagePublic {
-	
-Messagepublic getMessageByObjet(Objet objet) throws Exception;
+
+	List<Messagepublic> getMessageByObjet(Objet objet) throws Exception;
+
+	List<Messagepublic> getReponses(Messagepublic messagepublic) throws Exception;
 }

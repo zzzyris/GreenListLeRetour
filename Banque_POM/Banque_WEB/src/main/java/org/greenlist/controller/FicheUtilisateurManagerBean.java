cf2dfa9361a2073c5c0bb7e74de9164044c3637c
markus <markus@DESKTOP-JPLSIMA.form-domain.lan>
2017-05-05 15:12:34 +0200

    MAJ bean ( get/set)

--- Banque_POM/Banque_WEB/src/main/java/org/greenlist/controller/FicheUtilisateurManagerBean.java
+++ Banque_POM/Banque_WEB/src/main/java/org/greenlist/controller/FicheUtilisateurManagerBean.java
@@ -8,6 +8,10 @@ import javax.faces.bean.ViewScoped;
 import org.greenlist.business.api.IBusinessUtilisateur;
 import org.greenlist.entity.Utilisateur;
 
+/**
+ * @author markus
+ *
+ */
 @ManagedBean(name = "mbFicheUtilisateur")
 @ViewScoped
 public class FicheUtilisateurManagerBean {
@@ -35,4 +39,77 @@ public class FicheUtilisateurManagerBean {
 		nbAVis = proxyUtilisateur.recupererNbAvis(utilisateurAffiche);
 		
 	}
+
+
+	public IBusinessUtilisateur getProxyUtilisateur() {
+		return proxyUtilisateur;
+	}
+
+
+	public void setProxyUtilisateur(IBusinessUtilisateur proxyUtilisateur) {
+		this.proxyUtilisateur = proxyUtilisateur;
+	}
+
+
+	public Utilisateur getUtilisateurAffiche() {
+		return utilisateurAffiche;
+	}
+
+
+	public void setUtilisateurAffiche(Utilisateur utilisateurAffiche) {
+		this.utilisateurAffiche = utilisateurAffiche;
+	}
+
+
+	public int getMoyenne() {
+		return moyenne;
+	}
+
+
+	public void setMoyenne(int moyenne) {
+		this.moyenne = moyenne;
+	}
+
+
+	public int getNbAVis() {
+		return nbAVis;
+	}
+
+
+	public void setNbAVis(int nbAVis) {
+		this.nbAVis = nbAVis;
+	}
+
+
+	public int getNbObjets() {
+		return nbObjets;
+	}
+
+
+	public void setNbObjets(int nbObjets) {
+		this.nbObjets = nbObjets;
+	}
+
+
+	public int getNbSouhaits() {
+		return nbSouhaits;
+	}
+
+
+	public void setNbSouhaits(int nbSouhaits) {
+		this.nbSouhaits = nbSouhaits;
+	}
+
+
+	public int getNbEchanges() {
+		return nbEchanges;
+	}
+
+
+	public void setNbEchanges(int nbEchanges) {
+		this.nbEchanges = nbEchanges;
+	}
+	
+	
+	
 }
